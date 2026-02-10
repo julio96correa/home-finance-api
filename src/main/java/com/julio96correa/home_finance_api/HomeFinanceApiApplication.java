d48c8b171c2cef09c606281fed615e52c811c021
julio96correa <julio96correa@gmail.com>
2026-02-10 15:09:12 -0500

    feat: initial spring boot project structure

---
+++ src/main/java/com/julio96correa/home_finance_api/HomeFinanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.julio96correa.home_finance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeFinanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeFinanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julio96correa/home_finance_api/HomeFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julio96correa.home_finance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeFinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
